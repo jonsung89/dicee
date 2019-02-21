647b511579afbc10b056da5820cef1c0171a81d8
Jon Sung <jonsung89@gmail.com>
2019-02-21 01:40:56 -0600

    added motion detect function to roll dice

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -15,14 +15,11 @@ class ViewController: UIViewController {
     var randomDiceIndex1: Int = 0
     var randomDiceIndex2: Int = 0
     
-    
     @IBOutlet weak var diceImageView1: UIImageView!
-    
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         updateDiceImages()
     }
     
@@ -30,14 +27,16 @@ class ViewController: UIViewController {
         updateDiceImages()
     }
     
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
     func updateDiceImages() {
-        
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
         
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
-        
     }
 
 }
